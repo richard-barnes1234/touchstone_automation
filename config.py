860393257177166b8e377b8e973dff1df0903693
antoneythuo <antoney2236@gmail.com>
2026-06-03 21:30:25 +0300

    added config

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py  Store all credentials and settings here
+
+TOUCHSTONE_URL = "https://crcins-na-prod-touchstoneapi.air-worldwide.com/FEP/AirServiceFacade.svc"
+USERNAME = "automationservice"
+PASSWORD = "tBaecj*6Iep$N3^C"
+DOMAIN = "crcins.air"
+
+# Key IDs we discovered during API testing
+SQL_INSTANCE_SID = 1
+BUSINESS_UNIT_SID = 1
+DATA_SOURCE_SID = 2
