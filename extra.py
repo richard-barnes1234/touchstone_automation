a2a5d76019299aae75341671569ca90dee7fe704
antoneythuo <antoney2236@gmail.com>
2026-06-03 23:54:28 +0300

    updated test1

--- extra.py
+++ extra.py
@@ -1,6 +1,8 @@
-# test_analysis_type.py
+# test_haz_type.py
 from get_analysis_sids import get_analyses_for_project
 
-analyses = get_analyses_for_project('1', 'DefaultUW')
+# National26 - Hazard Analysis project
+analyses = get_analyses_for_project('16726', 'Kistler')
+print(f"Found {len(analyses)} analyses\n")
 for a in analyses:
     print(f"SID {a['AnalysisSid']}  Type: {a['AnalysisType']}  {a['AnalysisName']}")
