e3a249a51b00512f536dac9f6b2f9f0781829f84
Richard Barnes <richard.barnes+crcgroup@crcgroup.com>
2026-06-18 10:34:03 -0400

    mode codes

--- config.py
+++ config.py
@@ -9,3 +9,18 @@ DOMAIN = "crcins.air"
 SQL_INSTANCE_SID = 1
 BUSINESS_UNIT_SID = 1
 DATA_SOURCE_SID = 2
+
+
+# automation-dev
+# TOUCHSTONE_URL_DEV = "https://crcins-na-dev-touchstoneapi.air-worldwide.com/FEP/AirServiceFacade.svc"
+# USERNAME_DEV = "automationservice"
+# PASSWORD_DEV = "wiT&qBr2rfY@V4Ey"
+# DOMAIN_DEV = "crcins.air"
+
+# automation-prod
+# TOUCHSTONE_URL_PROD = "https://crcins-na-prod-touchstoneapi.air-worldwide.com/FEP/AirServiceFacade.svc"
+# USERNAME_PROD = "automationservice"
+# PASSWORD_PROD = "jnsAowoM1ws!qYUt"
+# DOMAIN_PROD = "crcins.air"
+
+# https://crcins-na-prod-touchstoneapi.air-worldwide.com/fep/airservicefacade.svc
